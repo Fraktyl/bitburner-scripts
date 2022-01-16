cd620fddc83526c2ff216bd622098d3bacdc7292
Fraktyl <jef@fraktyl.net>
2022-01-15 23:37:00 -0500

    rename from common.js

---
+++ src/constants.js
@@ -0,0 +1,23 @@
+export const settings = {
+  homeRamReserved: 20,
+  homeRamReservedBase: 20,
+  homeRamExtraRamReserved: 64,
+  homeRamBigMode: 64,    
+  minSecurityLevelOffset: 2,
+  maxMoneyMultiplayer: 0.9,
+  minSecurityWeight: 100,
+  mapRefreshInterval: 24 * 60 * 60 * 1000,
+  maxWeakenTime: 30 * 60 * 1000,
+
+}
+
+export const changes = {
+  hack: 0.002,
+  grow: 0.004,
+  weaken: 0.05,  
+}
+export const keys = {
+  serverMap : 'BB_SERVER_MAP',
+  reserve: 'BB_RESERVE',
+  
+}
